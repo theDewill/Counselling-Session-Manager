89899af6eb0a73366fa93a8f8a2da539415e2e6a
Nomin Sendinu <soloriderns@gmail.com>
2024-07-19 14:05:40 +0530

    Mv: moved the safe content to envrironment and loaded from process

--- firebaseConf.ts
+++ firebaseConf.ts
@@ -3,16 +3,16 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
-const firebaseConfig = {
-  apiKey: "AIzaSyCLZlbPBN0P4HI2YWs_KeF6ARIxf8pyPAY",
-  authDomain: "sitebackend-2f34e.firebaseapp.com",
-  projectId: "sitebackend-2f34e",
-  storageBucket: "sitebackend-2f34e.appspot.com",
-  messagingSenderId: "94688703117",
-  appId: "1:94688703117:web:326f328878fc5558f8e8c4",
-  measurementId: "G-MRRQK00C2K",
-};
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
